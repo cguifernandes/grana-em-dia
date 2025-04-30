eb2fc22899005cdecc4c3d5fe5334c4247b0e325
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2025-04-30 19:45:10 -0300

    feat: configure shadcn

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -4,8 +4,8 @@ const Home = () => {
 	return (
 		<>
 			<Head title="Home" />
-			<main className="bg-black min-h-screen w-full">
-				<h1 className="text-white">Bem-vindo</h1>
+			<main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-50 to-blue-50 p-4">
+				<h1 className="text-black">Bem-vindo</h1>
 			</main>
 		</>
 	);

e2e9e6793707218363ca444e553e520475be43ee
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2025-05-02 22:13:30 -0300

    feat: redirect if authenticated middleware

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+class RedirectIfAuthenticated extends Middleware
+{
+  public function handle(Request $request, Closure $next, ...$guards)
+  {
+      foreach ($guards as $guard) {
+          if (Auth::guard($guard)->check()) {
+              return redirect('/dashboard'); 
+          }
+      }
+
+      return $next($request);
+  }
+}
